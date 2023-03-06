3c7df6e5a106dcc221898521783afed8ef78688f
mezencev <33099967+Mezencev@users.noreply.github.com>
2023-03-06 15:23:51 +0200

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,45 +1,37 @@
-const MAX_VISIBLE_TEXT_ELEMENTS_COUNT = 6; // 2 elements per 3 lines each
-const RICH_CONTENT_TYPE = "richContent";
-// HTML DATA ATTRIBUTES:
-const ATTRIBUTE_NAME = "data-lp-point";
-const ENGAGEMENT_AREA = "engagement_area";
-const LINES_AREA = "lines_area";
-// HTML IDS:
-const LP_LINE_ID = "lp_line_";
-
-const getAttribute = (value, name = ATTRIBUTE_NAME, matchLike = false) => {
+lpTag.external = lpTag.external || {};
+const getAttribute = (value, name = "data-lp-point", matchLike = false) => {
   return `${name}${matchLike ? "^" : ""}='${value}'`;
 };
-
-try {
-  const loadListener = () => {
-    lpTag.hooks.push({
-      name: "AFTER_GET_LINES",
-      callback: function ({ data }) {
-        if (!data.lines.length) return;
-
+lpTag.external.hook = {
+  // handle incoming messages
+  afterGetLines: function (data) {
+    try {
+      if (data.data.lines.length > 0) {
         const lastLineIdx = data.lines.length - 1;
         const lastLine = data.lines[lastLineIdx];
-        if (!lastLine || lastLine.type !== RICH_CONTENT_TYPE || lastLine.text.elements.length < MAX_VISIBLE_TEXT_ELEMENTS_COUNT) return;
+        if (!lastLine || lastLine.type !== "richContent" || lastLine.text.elements.length < 6) return;
 
-        const lineElements = document.querySelectorAll(`div[${getAttribute(LINES_AREA)}] > div[${getAttribute(LP_LINE_ID, "id", true)}]`);
-        const engagementAreaElement = document.querySelector(`div[${getAttribute(ENGAGEMENT_AREA)}]`);
+        const lineElements = document.querySelectorAll(`div[${getAttribute("lines_area")}] > div[${getAttribute("lp_line_", "id", true)}]`);
+        const engagementAreaElement = document.querySelector(`div[${getAttribute("engagement_area")}]`);
 
         // Disabling scroll
         engagementAreaElement.style.overflow = "hidden";
 
         setTimeout(() => {
           const idx = lastLineIdx || lineElements.length;
-          const prevMessageElement = document.querySelector(`div[${getAttribute(`${LP_LINE_ID}${idx - 1}`, "id")}]`);
+          const prevMessageElement = document.querySelector(`div[${getAttribute(`${"lp_line_"}${idx - 1}`, "id")}]`);
 
           engagementAreaElement.style.overflow = "auto";
           engagementAreaElement.scrollTop = prevMessageElement.offsetTop - 100;
         }, 1000);
-      },
-    });
-  };
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
 
-  document.addEventListener("DOMContentLoaded", loadListener);
-} catch (e) {
-  console.dir(e.message);
-}
+lpTag.hooks.push({
+  name: "AFTER_GET_LINES",
+  callback: lpTag.external.hook.afterGetLines,
+});
